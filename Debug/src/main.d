538f419bbb2ff286a551f9d41e5fd0b4abdde905
Paul Wozny <pawel0wozny@gmail.com>
2018-03-30 12:15:04 +0200

    added eclipse project itself

---
+++ Debug/src/main.d
@@ -0,0 +1,13 @@
+src/main.o: ../src/main.c \
+ /home/pawel/workspace/eclipse/SPICY/headers/main.h \
+ /home/pawel/workspace/eclipse/SPICY/headers/constArgs.h \
+ /home/pawel/workspace/eclipse/SPICY/headers/netlistParser.h \
+ /home/pawel/workspace/eclipse/SPICY/headers/main.h
+
+/home/pawel/workspace/eclipse/SPICY/headers/main.h:
+
+/home/pawel/workspace/eclipse/SPICY/headers/constArgs.h:
+
+/home/pawel/workspace/eclipse/SPICY/headers/netlistParser.h:
+
+/home/pawel/workspace/eclipse/SPICY/headers/main.h:
---
+++ Debug/src/netlistParser.d
@@ -0,0 +1,10 @@
+src/netlistParser.o: ../src/netlistParser.c \
+ /home/pawel/workspace/eclipse/SPICY/headers/netlistParser.h \
+ /home/pawel/workspace/eclipse/SPICY/headers/main.h \
+ /home/pawel/workspace/eclipse/SPICY/headers/constArgs.h
+
+/home/pawel/workspace/eclipse/SPICY/headers/netlistParser.h:
+
+/home/pawel/workspace/eclipse/SPICY/headers/main.h:
+
+/home/pawel/workspace/eclipse/SPICY/headers/constArgs.h:
